18db551bd00a098195a78594cfef7caad96037bf
Jack Elinzano <113414920+jcklzn@users.noreply.github.com>
2025-04-07 00:07:29 +0800

    initial springboot setup

---
+++ src/main/java/group3/p4enlistment/Application.java
@@ -0,0 +1,14 @@
+package group3.p4enlistment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/group3/p4enlistment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.p4enlistment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
